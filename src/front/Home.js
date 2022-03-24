2da96471efe874ed267a7a1f4f703581f96e8fc4
Marc Nadal <marcnadal7@gmail.com>
2022-03-25 00:15:18 +0100

    Intento de verificar el request de la API

--- src/front/Home.js
+++ src/front/Home.js
@@ -4,22 +4,38 @@ import {Link} from 'react-router-dom'
 const Home = () => {
 
     const [equipo, setEquipo] = useState([]);
-
-    useEffect(()=>{
+    const [equipo2, setEquipo2] = useState();
+    /*useEffect(()=>{
         // console.log('useEffect')
         obtenerDatos()
+    }, [])*/
+    
+    useEffect(()=>{
+        const url='localhost:8080/Api/Sub'
+        const peticion =fetch(url)
+        peticion
+        .then(datos=>datos.json())
+        .then(lectura=>{
+            console.log(lectura.results[0])
+        })
+        .catch((e)=>{console.log(e)})
     }, [])
 
     const obtenerDatos = async () => {
-        const data = await fetch('localhost:8080/Api/User')
+        const data = await fetch('localhost:8080/Api/Sub')
         const users = await data.json()
-        //console.log(users)
+        setEquipo2(data)
+        console.log(data)
+        console.log(users)
         setEquipo(users)
     }
+
+
 
     return(
         <>
             <h1>Nosotros</h1>
+            <h3>{equipo2}</h3>
             <ul>
                 {
                     equipo.map(item=>(
